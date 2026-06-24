00d0d03ab653666c624dbe97a4601968e4a39879
Andy <andreas.student2024@gmail.com>
2026-06-24 11:44:08 +0200

    first commit

--- main.c
+++ main.c
@@ -6,8 +6,26 @@ int main()
     // The Line below must *NOT* be changed or test will fail!
     srand(1);
 
-   
-    // Your code goes here
+    int rolls[100];
+    int counts[6] = {0};
+    int sum = 0;
+
+    for (int i = 0; i < 100; i++)
+    {
+        rolls[i] = rand() % 6 + 1;
+        counts[rolls[i] - 1]++;
+        sum += rolls[i];
+    }
+
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d\n", counts[i]);
+    }
+
+    printf("%d\n", sum);
+
+    float average = (float)sum / 100;
+    printf("%.1f\n", average);
 
     return 0;
 }
